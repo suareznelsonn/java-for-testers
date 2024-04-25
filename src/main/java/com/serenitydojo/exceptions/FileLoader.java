f804d90fd798750c6836988014513cba9893291f
suareznelsonn <suareznelsonn@gmail.com>
2024-04-24 20:35:09 -0500

    Update FileLoader.java
    Exercise 12

--- src/main/java/com/serenitydojo/exceptions/FileLoader.java
+++ src/main/java/com/serenitydojo/exceptions/FileLoader.java
@@ -1,19 +1,35 @@
 package com.serenitydojo.exceptions;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 public class FileLoader {
     public String readHelloWorld() throws IOException {
-        return "";//Files.readString(Paths.get("src/main/resources/hello.txt"));
+        try {
+            return Files.readString(Paths.get("src/main/resources/hello.txt"));
+        } catch(NoSuchFileException noSuchFile){
+            String s = "Exception : File not found";
+            return s;
+        }
     }
 
-    public Boolean fileContainsText(String filename, String expectedText) {
+    public boolean fileContainsText(String filename, String expectedText) throws IOException {
         String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+        try {
+            return (Files.readString(Paths.get(path)).contains(expectedText));
+        } catch (IOException e) {
+            return false;
+        }
     }
 
-    public Boolean fileHasText(String filename, String expectedText) {
+    public boolean fileHasText(String filename, String expectedText) throws IOException, RuntimeException {
         String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+        try {
+            return (Files.readString(Paths.get(path)).contains(expectedText));
+        } catch (NoSuchFileException noSuchFile) {
+            throw new MissingWelcomeFileException("Missing welcome file: " + filename, noSuchFile);
+        }
     }
 }
